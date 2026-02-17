1512a1b0d7c70d17930372fadb8150f1269ea86a
DanielCBruni <daniel.c.bruni@gmail.com>
2026-02-17 16:43:12 -0600

    Added 2 queries, one for menu count and one for menu count without addons.

--- group_queries.sql
+++ group_queries.sql
@@ -1,3 +1,5 @@
 SELECT SUM(total_amount) FROM Orders; --Shows sales are >1M 
 SELECT MAX(order_timestamp) - MIN(order_timestamp) AS interval_difference FROM Orders;  --Shows that we have 52 weeks of orders
 SELECT DATE(order_timestamp), SUM(total_amount) FROM Orders GROUP BY DATE(order_timestamp) ORDER BY SUM(total_amount) DESC LIMIT 4;  --Shows that we have precisely 3 peak days with the next being a good amount in profit away
+SELECT COUNT(*) AS count_menu_items FROM menu_items; --Enumerates all menu items
+SELECT COUNT(*) AS count_menu_items FROM menu_items WHERE item_type = 'Drink'; --Enumerates all menu items EXLCUDING ADDONS
