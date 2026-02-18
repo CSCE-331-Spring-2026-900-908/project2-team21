29addc18fa414dd1ea0e956f705df165a67301b3
Sidharth Kammili <sidharthkammili@gmail.com>
2026-02-17 19:22:17 -0600

    Add query: top selling drinks

--- group_queries.sql
+++ group_queries.sql
@@ -6,3 +6,4 @@ SELECT COUNT(*) AS count_menu_items FROM menu_items WHERE item_type = 'Drink'; -
 SELECT COUNT(*) AS count_employees FROM employees; --Enumerates count of employees
 SELECT * FROM inventory WHERE quantity_in_stock < reorder_level; --Shows all items in inventory that need to be reordered
 SELECT COUNT(CASE WHEN total_amount < 10 THEN 1 END) AS "<$10", COUNT(CASE WHEN total_amount BETWEEN 10 AND 20 THEN 1 END) AS "$10-$20", COUNT(CASE WHEN total_amount > 20 THEN 1 END) AS "$20+" FROM orders; --Shows the count of orders in different categories of sales amount
+SELECT m.item_name, SUM(oli.quantity) AS total_sold FROM order_line_items oli JOIN menu_items m ON m.menu_item_id = oli.menu_item_id WHERE m.item_type = 'Drink' GROUP BY m.item_name ORDER BY total_sold DESC LIMIT 10; --Top 10 best-selling drinks by quantity
