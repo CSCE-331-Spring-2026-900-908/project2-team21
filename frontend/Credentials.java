f50c74527d54cfe503eed3eaa277ff68401d202c
DanielCBruni <daniel.c.bruni@gmail.com>
2026-02-23 16:27:39 -0600

    Updated Credentials.

--- frontend/Credentials.java
+++ frontend/Credentials.java
@@ -1,8 +1,8 @@
 package frontend;
 
 public class Credentials {
-    public static final String username = "team_21";
-    public static final String password = "21";
+    public static final String username = "USERNAME";
+    public static final String password = "PASSWORD";
 }
 
 // HOW TO WORK:
